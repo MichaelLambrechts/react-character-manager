38544990425f599b9dd41417a3c42ff93b2c1346
Michael Lambrechts <miclambrechts@gmail.com>
2019-03-26 17:00:57 +0100

    ajout nodemodules

---
+++ build/precache-manifest.2fcfddf4713b1c30e158fe1989af1df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd2f340ed4a1a9060846",
+    "url": "/react-character-manager/static/css/main.9d2130a2.chunk.css"
+  },
+  {
+    "revision": "fd2f340ed4a1a9060846",
+    "url": "/react-character-manager/static/js/main.4bd6e12e.chunk.js"
+  },
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "acf5321c890452d6bc91",
+    "url": "/react-character-manager/static/css/2.e7237872.chunk.css"
+  },
+  {
+    "revision": "acf5321c890452d6bc91",
+    "url": "/react-character-manager/static/js/2.b9ab9022.chunk.js"
+  },
+  {
+    "revision": "a4739888ddd593e641603f359a9eed37",
+    "url": "/react-character-manager/index.html"
+  }
+];
